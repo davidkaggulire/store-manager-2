c6d8ce257e4addea7d02d206d921780bea7ac418
davidkaggulire <dkaggulire@gmail.com>
2018-10-19 14:54:57 +0300

    [feat]Create addsale endpoint.[#161247571]

---
+++ api/views/sales_views.py
@@ -0,0 +1,38 @@
+from api import app
+from flask import jsonify, request, make_response
+from api.models.products import Products
+from api.models.sales import Sales
+from api.models.user import User
+from api.models.admin import Admin
+from api.models.attendant import Attendant
+from api.views.product_views import PRODUCT_LIST
+
+SALES_LIST = []
+
+user = User(1, username='attendant')
+admin_user = Admin(1,username='admin')
+attendant_user = Attendant(1, 'attendant')
+
+@app.route('/api/v1/sales', methods=['GET', 'POST'])
+def post_sales():
+    """
+    route to add a new sale
+    """
+    if request.method == 'POST':
+        form_data = request.get_json(force=True)
+        # we need to find that the product being bought is in the store.
+        # so we need to figure out a way to do that 
+        # can only make a sale when product is in store.
+        product_name = form_data['product_name']
+        sale_id = len(SALES_LIST) + 1
+
+        attendant_id = attendant_user.user_id
+        
+        if product_name != "":
+            save_sale = attendant_user.add_sale(sale_id, PRODUCT_LIST, SALES_LIST,attendant_id, product_name)
+            if save_sale:
+                return make_response(jsonify({'message': save_sale}), 201)
+            else:
+                return make_response(jsonify({'message': 'Product not in store'}), 404)
+        else:
+            return make_response(jsonify({'message': 'Product name missing'}), 406)

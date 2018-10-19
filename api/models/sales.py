bbef4212e01995a4e1607b9ffcd027289e8de03d
davidkaggulire <dkaggulire@gmail.com>
2018-10-19 13:02:21 +0300

    Adds a class to define a sale

---
+++ api/models/sales.py
@@ -0,0 +1,23 @@
+"""
+module sales
+"""
+
+from flask import jsonify, make_response
+
+class Sales():
+    """
+    class for creating a saleorder
+    """
+    def __init__(self, sale_id, product_id, username):
+        """
+        initializing class attributes
+        """
+        self.sale_id = sale_id
+        self.product_id = product_id
+        self.username = username
+
+    def __str__(self):
+        """
+        returns string representation of attributes
+        """
+        return '{}'.format(self.sale_id)

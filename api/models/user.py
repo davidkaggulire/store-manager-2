7a294f712bfa773c637cd66738706c7442746fee
davidkaggulire <dkaggulire@gmail.com>
2018-10-19 13:50:40 +0300

    Adds function to get products

--- api/models/user.py
+++ api/models/user.py
@@ -18,3 +18,16 @@ class User():
     def __str__(self):
         """return string rep of class"""
         return '{}'.format(self.username)
+
+    @staticmethod
+    def get_products(product_list):
+        """
+        returns all products in product_list
+        """
+        if len(product_list) == 0:
+            result = {
+                'message': 'Please add products to store'
+            }
+            return result
+        else:
+            return product_list

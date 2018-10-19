34b0b934d7f0fd60a0740851b149c6ce3914601c
davidkaggulire <dkaggulire@gmail.com>
2018-10-19 14:54:13 +0300

    [feat]Create addsale endpoint.[#161247571]

--- api/models/attendant.py
+++ api/models/attendant.py
@@ -22,3 +22,25 @@ class Attendant(User):
         returning str repr for class
         """
         return '{}'.format(self.username)
+
+    @staticmethod
+    def add_sale(sale_id, product_list, sales_list, attendant_id, product_name):
+        """
+        method to add a sale order
+        """
+        # parsing in an attendant from whom Id is gotten
+        for product in product_list:
+            if product['product_name'] == product_name:
+                response = {
+                    'sale_id': sale_id,
+                    'product_id': product['product_id'],
+                    'product_name': product['product_name'],
+                    'category': product['category'],
+                    'price': product['price'],
+                    'quantity': product['quantity'],
+                    'attendant_id': attendant_id
+                }
+                
+                sales_list.append(response)
+                
+                return 'Sale made successfully'

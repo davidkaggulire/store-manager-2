2b53bb13d96e5e781f9d5fa7ad4a760a67b3fc40
davidkaggulire <dkaggulire@gmail.com>
2018-10-23 11:31:39 +0300

    Refactors code

--- api/views/product_views.py
+++ api/views/product_views.py
@@ -16,45 +16,50 @@ attendant_user = Attendant(1, 'attendant')
 @app.route('/')
 def home():
     """Home route"""
-    return "Welcome to Store Manager"
+    return make_response(jsonify({'message':'Welcome to Store Manager'}), 200)
 
-@app.route('/api/v1/products', methods = ['GET', 'POST'])
+@app.route('/api/v1/products', methods = ['POST'])
 def post_products():
     """
     method to handle post and get requests
     """
-    if request.method == 'POST':
-        product_id = len(PRODUCT_LIST) + 1
-        form_data = request.get_json(force=True)
-        product_name = form_data['product_name']
-        category = form_data['category']
-        price = form_data['price']
-        quantity = form_data['quantity']
-        minimum_quantity = form_data['minimum_quantity']
+    product_id = len(PRODUCT_LIST) + 1
+    form_data = request.get_json(force=True)
+    product_name = form_data['product_name']
+    category = form_data['category']
+    price = form_data['price']
+    quantity = form_data['quantity']
+    minimum_quantity = form_data['minimum_quantity']
 
-        if product_name != "" and category != "" and price != "" and quantity != "" and minimum_quantity != "" :
-            product = Products(product_id=product_id, product_name=product_name, category=category,
-        price=price, quantity=quantity, minimum_quantity=minimum_quantity)
-            new_post = Admin.add_product(product, PRODUCT_LIST)
-            return make_response(jsonify({"message": new_post}), 201)
-        else:
-            return make_response(jsonify({'message': 'Missing input fields'}), 400)
+    if product_name != "" and category != "" and price != "" and quantity != "" and minimum_quantity != "" :
+        product = Products(product_id=product_id, product_name=product_name, category=category,
+    price=price, quantity=quantity, minimum_quantity=minimum_quantity)
+        new_post = Admin.add_product(product, PRODUCT_LIST)
+        return make_response(jsonify({"message": new_post}), 201)
+    else:
+        return make_response(jsonify({'message': 'Missing input fields'}), 400)
 
-    elif request.method == 'GET':
-        new_get = Admin.get_products(PRODUCT_LIST)
-        return make_response(jsonify(new_get), 200)
+@app.route('/api/v1/products')
+def get_products():
+    new_get = Admin.get_products(PRODUCT_LIST)
+    return make_response(jsonify(new_get), 200)
 
-@app.route('/api/v1/products/<int:product_id>', methods=['GET', 'POST'])     
+@app.route('/api/v1/products/<int:product_id>')     
 def get_product(product_id):
     """
     route to get specific product by Id
     """
-    if request.method == 'GET':
-        specific_product = Attendant.get_specific_product(product_id, PRODUCT_LIST)
-        if specific_product:
-            return make_response(jsonify(specific_product), 200)
-        else:
-            return make_response(jsonify({'message': 'Product not found'}), 404)
+    specific_product = Attendant.get_specific_product(product_id, PRODUCT_LIST)
+    if specific_product:
+        return make_response(jsonify(specific_product), 200)
+    else:
+        return make_response(jsonify({'message': 'Product not found'}), 404)
 
-    elif request.method == 'POST':
-        return make_response(jsonify({'message': 'Unallowed route'}), 405)
+@app.route('/api/v1/products/<int:product_id>', methods=['POST'])
+def post_product(product_id):
+    """route to post a product to an Id"""
+    return make_response(jsonify({'message': 'Unallowed route'}), 400)
+
+@app.route('/api/v1/products/', methods = ['POST'])
+def post_wrong_url_products():
+    return make_response(jsonify({'message': 'Unallowed route'}), 400)

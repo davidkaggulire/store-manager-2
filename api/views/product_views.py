eedb9b7f40c955e35d5af64864644457943b3316
davidkaggulire <dkaggulire@gmail.com>
2018-10-24 22:24:12 +0300

    Edits product views

--- api/views/product_views.py
+++ api/views/product_views.py
@@ -9,14 +9,6 @@ from api.models.attendant import Attendant
 
 PRODUCT_LIST = [
     {
-        "product_name": "pianobag",
-        "category": "bags",
-        "price": 30000,
-        "quantity": 10,
-        "minimum_quantity": 6,
-        "product_id": 3
-    },
-    {
         "product_name": "guitar",
         "category": "guitars",
         "price": 500000,

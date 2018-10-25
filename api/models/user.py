75a5e9578da6a46b9100902962247f7280b14abc
davidkaggulire <dkaggulire@gmail.com>
2018-10-25 17:40:49 +0300

    Modifies user

--- api/models/user.py
+++ api/models/user.py
@@ -17,10 +17,6 @@ class User():
         self.username = username
         self.admin_status = bool(admin_status)
 
-    def __str__(self):
-        """return string rep of class"""
-        return '{}'.format(self.username)
-
     @staticmethod
     def get_products(product_list):
         """

b0deb4dc9a066c07679bc927a9aaaa280a842673
davidkaggulire <dkaggulire@gmail.com>
2018-10-30 19:29:08 +0300

    Edits init.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,14 @@
 """__init__.py"""
 
+import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object('config.DevelopmentConfig')
 from api.views import product_views
+from api.views import user_views
+
+def create_app(app):
+  """setting flask app"""
+  config_name = os.getenv('FLASK_CONFIGURATION', 'default')
+  app.config.from_object('config.DevelopmentConfig')
+  

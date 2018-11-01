b1dc82ab662c8f2ca88715883b8ab206d24287cd
davidkaggulire <dkaggulire@gmail.com>
2018-11-01 20:37:41 +0300

    Edits user files

--- api/models/user.py
+++ api/models/user.py
@@ -2,15 +2,14 @@
 user.py
 """
 
-from api.models.db import Database
 
 class User:
-  """class for user"""
-  def __init__(self, firstname, lastname, username, password):
-    """
-    initialize class attributes
-    """
-    self.firstname = firstname
-    self.lastname = lastname
-    self.username = username
-    self.password = password
+    """class for user"""
+    def __init__(self, firstname, lastname, username, password):
+        """
+        initialize class attributes
+        """
+        self.firstname = firstname
+        self.lastname = lastname
+        self.username = username
+        self.password = password

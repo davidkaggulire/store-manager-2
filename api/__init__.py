f8cf181dff9f1b3d8343c48ba2e8eb263ade019c
davidkaggulire <dkaggulire@gmail.com>
2018-11-02 00:28:55 +0300

    [#161640559]Chore to lint code

--- api/__init__.py
+++ api/__init__.py
@@ -1,28 +1,11 @@
 """__init__.py"""
 
-import os
 from flask import Flask
 from config import app_config
 from flask_jwt_extended import JWTManager
 from api.views.product_views import product
 from api.views.sales_views import sales
 from api.views.user_views import userpage
-
-
-# app = Flask(__name__)
-# app.config['JWT_SECRET_KEY'] = 'davidsecret123'
-# jwt = JWTManager(app)
-# from api.models.db import Database
-# db = Database()
-
-# db.create_user_table()
-# db.create_products_table()
-# db.create_sales_table()
-
-
-# from api.views import product_views
-# from api.views import user_views
-# from api.views import sales_views
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -42,4 +25,3 @@ def create_app(config_name):
     app.register_blueprint(userpage)
   
     return app
-

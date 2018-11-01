6155e91b19f432226f32cbed5d7a8cefd32bba07
davidkaggulire <dkaggulire@gmail.com>
2018-11-01 20:35:56 +0300

    Edits products file to use Flake8

--- api/models/products.py
+++ api/models/products.py
@@ -2,15 +2,16 @@
 
 import datetime
 
+
 class Products:
-  """creates a product"""
-  def __init__(self, product_name, category, price, quantity, minimum_quantity):
-    """
-    initializing attributes for class
-    """
-    self.product_name = product_name
-    self.category = category
-    self.price = price
-    self.quantity = quantity
-    self.minimum_quantity = minimum_quantity
-    self.date_added = datetime.datetime.now()
+    """creates a product"""
+    def __init__(self, product_name, category, price, quantity, minimum_quantity):
+        """
+        initializing attributes for class
+        """
+        self.product_name = product_name
+        self.category = category
+        self.price = price
+        self.quantity = quantity
+        self.minimum_quantity = minimum_quantity
+        self.date_added = datetime.datetime.now()

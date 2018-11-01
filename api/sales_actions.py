a3ea7b04f3664437cfa491accd97b6898863c1d7
davidkaggulire <dkaggulire@gmail.com>
2018-11-01 20:39:49 +0300

    Edits sale controller

--- api/sales_actions.py
+++ api/sales_actions.py
@@ -12,11 +12,27 @@ class Sales_Controller:
     handles actions to be implemented from models to views
     """
     @staticmethod
-    def create_sale(product_name, price, quantity):
+    def create_sale(product_name, quantity, total, user_id, product_id):
         """
         method to register a user
         """
-        query = "INSERT INTO sales(product_name, price, quantity) VALUES(\
-        '{}', '{}', '{}', '1', '1')".format(product_name, price, quantity)
-        cursor.execute(query)
+        cursor.execute("INSERT INTO sales(product_name, quantity, total, user_id, product_id) VALUES(\
+        %s, %s, %s, %s, %s)",(product_name, quantity, total, user_id, product_id))
         return True
+
+    @staticmethod
+    def get_sales():
+        """"
+        method to return all sales made
+        """
+        dict_cursor.execute("SELECT * FROM sales")
+        return dict_cursor.fetchall()
+
+
+    @staticmethod
+    def get_single_sale(sale_id):
+        """
+        method to return single sale
+        """
+        cursor.execute("SELECT * FROM sales WHERE sale_id=%s",(sale_id,))
+        return cursor.fetchone()

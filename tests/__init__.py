996a05e2a0202f0fe221fbb86292e631f0614221
davidkaggulire <dkaggulire@gmail.com>
2018-11-01 20:46:24 +0300

    Adds tests data

---
+++ tests/__init__.py
@@ -0,0 +1,21 @@
+"""module init tests"""
+
+ADMIN_USER = {
+	"firstname": "don",
+	"lastname": "kaggulire",
+	"username":"don",
+	"password": "don1234!"
+}
+
+USER = {
+	"firstname": "david",
+	"lastname": "kaggulire",
+	"username":"dkaggs",
+	"password": "dkaggs123!"
+}
+
+LOGIN = {
+    "username":"don",
+	"password": "don1234!"
+}
+

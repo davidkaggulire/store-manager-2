a2e001dc7d25b8573a21305c2c7a140376eb5bd9
davidkaggulire <dkaggulire@gmail.com>
2018-11-05 00:00:48 +0300

    Edits test validators

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -3,6 +3,7 @@ module test_users
 """
 
 import unittest
+import re
 import json
 from api.validators import Validators
 from api import create_app
@@ -18,12 +19,15 @@ class TestValidators(unittest.TestCase):
         setting up test data 
         """ 
         app = create_app("testing")
-        self.client = app.test_client()
-        self.validate = Validators()
+        with app.app_context():    
+            self.client = app.test_client()
+            self.validate = Validators()
 
     def test_validate_product(self):
+        
         """test to ensure correct data"""
         self.assertEqual(self.validate.validate_input_number(PRODUCT['price']), None)
+        # self.assertRegex(self.validate.validate_input_number(str(PRODUCT['price'])), r'\d')
        
 if __name__ == '__main__':
     unittest.main()

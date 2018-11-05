4c34bd8df23777d8a5139d1b38103eb054446463
davidkaggulire <dkaggulire@gmail.com>
2018-11-05 23:29:34 +0300

    Edits controller to use database methods

--- api/controllers/user_controller.py
+++ api/controllers/user_controller.py
@@ -1,69 +1,41 @@
 """file to handle db operations"""
-import re
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask import jsonify
+
 from api.models.user import User
 from api.models.db import Database
 
-db = Database()
-cursor = db.cur
-dict_cursor = db.dict_cursor
-db.create_user_table()
 
-class UserController(User):
+class UserController:
     """
     class to handle user specific operations
     """
+    def __init__(self):
+        """
+        initialize variables
+        """
+        self.db_con = Database()
 
-
-
-    @staticmethod
-    def create_user(firstname, lastname, username, password):
+    def register_user(self, firstname, lastname, username, password):
         """
         method to register a user
         """
-        cursor.execute("INSERT INTO users(firstname, lastname, username, password, role) VALUES(\
-        %s, %s, %s, %s, 'attendant')", (firstname, lastname, username, 
-        generate_password_hash(password),))
-        return True
+        new_user = User(firstname, lastname, username, password)
+        self.db_con.create_user(firstname=new_user.firstname, lastname=new_user.lastname, username=new_user.username, password=new_user.password)
 
-    @staticmethod
-    def create_admin( firstname, lastname, username, password):
+    def register_admin(self, firstname, lastname, username, password):
         """
         method to register an admin
         """
-        cursor.execute("INSERT INTO users(firstname, lastname, username, password, role) VALUES(\
-        %s, %s, %s, %s, 'admin')", (firstname, lastname, username, generate_password_hash(password),))
-        return True
+        new_admin = User(firstname, lastname, username, password)
+        self.db_con.create_admin(firstname=new_admin.firstname, lastname=new_admin.lastname, username=new_admin.username, password=new_admin.password)
 
-    @staticmethod
-    def check_username(username):
+    def correct_username(self, username):
         """
         method to check if username exists
         """
-        dict_cursor.execute("SELECT * from users WHERE username=%s", (username,))
-        return dict_cursor.fetchone()
+        return self.db_con.check_username(username)
 
-    @staticmethod
-    def check_password(password, username):
+    def correct_password(self, password, username):
         """
         function to check user password
-        """ 
-        cursor.execute("SELECT password from users WHERE username=%s", (username,))
-        data = cursor.fetchall()
-        for d in data:
-            check = check_password_hash(d[0], password)
-            if check:
-                return True
-            else:
-                return False
-
-    @staticmethod
-    def get_user_by_role(username):
         """
-        get user by id
-        """
-        cursor.execute("SELECT role from users WHERE username=%s", (username,))
-        data = cursor.fetchall()
-        for id in data:
-            return id[0]
+        return self.db_con.check_password(password, username)

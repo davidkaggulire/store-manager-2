65d3c5a88b682359c39c04ff935433584ff55866
davidkaggulire <dkaggulire@gmail.com>
2018-11-06 01:57:29 +0300

    Edits tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -23,8 +23,9 @@ class TestUsers(TestBase):
         response = self.client.post('/api/v2/auth/login', content_type='application/json', data=json.dumps(LOGIN_ADMIN))
         msg = json.loads(response.data.decode())
         token = msg['user']
-        headers = {'content_type': 'application/json', 'Authorization': "Bearer " + token['auth_token']}
-        response = self.client.post('/api/v2/auth/signup', headers, data=json.dumps(USER))
+        
+        response = self.client.post('/api/v2/auth/signup', data=json.dumps(USER), 
+        headers = {'content_type': 'application/json', 'Authorization': "Bearer " + token['auth_token']})
         msg = json.loads(response.data.decode())
         self.assertIn('User created successfully', msg['message'])
         self.assertEqual(response.status_code, 201)
@@ -42,5 +43,5 @@ class TestUsers(TestBase):
         response = self.client.post('/api/v2/auth/admin', content_type='application/json', data=json.dumps(ADMIN_USER))
         response = self.client.post('/api/v2/auth/login', content_type='application/json', data=json.dumps(LOGIN_USER))
         msg = json.loads(response.data.decode())
-        self.assertIn('Wrong username or password, try again', msg['error'])
+        self.assertIn('Wrong username or password, try again', msg['message'])
         self.assertEqual(response.status_code, 401)

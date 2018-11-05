83287cf66d0ab4ecd2e6783ca71cbcac0ca8c585
davidkaggulire <dkaggulire@gmail.com>
2018-11-06 01:46:02 +0300

    Edits tests for users

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,12 +2,10 @@
 module test_users
 """
 
-import unittest
 import json
-from api.models.db import Database
-from api import create_app
-from . import ADMIN_USER, LOGIN_ADMIN, USER, PRODUCT, EMPTY_PRODUCT, LOGIN_USER
+from . import ADMIN_USER, LOGIN_ADMIN, USER, LOGIN_USER
 from tests.base_test import TestBase
+
 
 class TestUsers(TestBase):
     """
@@ -16,41 +14,33 @@ class TestUsers(TestBase):
 
     def test_creating_admin(self):
         """test method to post product if admin"""
-        response = self.client.post('/api/v2/auth/admin', content_type='application/json',
-        data=json.dumps(ADMIN_USER))
+        response = self.client.post('/api/v2/auth/admin', content_type='application/json', data=json.dumps(ADMIN_USER))
         self.assertEqual(response.status_code, 201)
 
     def test_creating_user(self):
         """test method to post product if attendant"""
-        response = self.client.post('/api/v2/auth/admin', content_type='application/json',
-        data=json.dumps(ADMIN_USER))
-        response = self.client.post('/api/v2/auth/login', content_type='application/json',
-        data=json.dumps(LOGIN_ADMIN))
+        response = self.client.post('/api/v2/auth/admin', content_type='application/json', data=json.dumps(ADMIN_USER))
+        response = self.client.post('/api/v2/auth/login', content_type='application/json', data=json.dumps(LOGIN_ADMIN))
         msg = json.loads(response.data.decode())
         token = msg['user']
-        print(token)
-        response = self.client.post('/api/v2/auth/signup', data=json.dumps(USER),
-        headers = {'content_type': 'application/json', 'Authorization': "Bearer "+ token['auth_token']})
+        headers = {'content_type': 'application/json', 'Authorization': "Bearer " + token['auth_token']}
+        response = self.client.post('/api/v2/auth/signup', headers, data=json.dumps(USER))
         msg = json.loads(response.data.decode())
         self.assertIn('User created successfully', msg['message'])
         self.assertEqual(response.status_code, 201)
 
     def test_login_admin_user(self):
         """test method to login admin_user"""
-        response = self.client.post('/api/v2/auth/admin', content_type='application/json',
-        data=json.dumps(ADMIN_USER))
-        response = self.client.post('/api/v2/auth/login', content_type='application/json',
-        data=json.dumps(LOGIN_ADMIN))
+        response = self.client.post('/api/v2/auth/admin', content_type='application/json', data=json.dumps(ADMIN_USER))
+        response = self.client.post('/api/v2/auth/login', content_type='application/json', data=json.dumps(LOGIN_ADMIN))
         msg = json.loads(response.data.decode())
         self.assertIn('User successfully logged in', msg['message'])
         self.assertEqual(response.status_code, 201)
-        
+
     def test_login_wrong_admin_user(self):
         """test method to check wrong_admin"""
-        response = self.client.post('/api/v2/auth/admin', content_type='application/json',
-        data=json.dumps(ADMIN_USER))
-        response = self.client.post('/api/v2/auth/login', content_type='application/json',
-        data=json.dumps(LOGIN_USER))
+        response = self.client.post('/api/v2/auth/admin', content_type='application/json', data=json.dumps(ADMIN_USER))
+        response = self.client.post('/api/v2/auth/login', content_type='application/json', data=json.dumps(LOGIN_USER))
         msg = json.loads(response.data.decode())
         self.assertIn('Wrong username or password, try again', msg['error'])
         self.assertEqual(response.status_code, 401)

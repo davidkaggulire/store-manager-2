8b28c753a9147bd6b727740e08196ffc66eea8e5
davidkaggulire <dkaggulire@gmail.com>
2018-11-06 11:39:00 +0300

    Adds sales controller

--- api/controllers/sales_controllers.py
+++ api/controllers/sales_controllers.py
@@ -1,38 +1,41 @@
 """sales_actions.py"""
 
 from api.models.db import Database
+from api.models.sales import Sales
 
-db = Database()
-cursor = db.cur
-dict_cursor = db.dict_cursor
-db.create_sales_table()
 
 class SalesController:
     """
     handles actions to be implemented from models to views
     """
-    @staticmethod
-    def create_sale(product_name, quantity, total, attendant_id, product_id):
+
+    def __init__(self):
+        """create initial db connection"""
+        self.db_con = Database()
+
+
+    def create_sale(self, product_name, quantity, total, attendant_id, product_id):
         """
         method to register a user
         """
-        cursor.execute("INSERT INTO sales(product_name, quantity, total, attendant_id, product_id) VALUES(\
-        %s, %s, %s, %s, %s)",(product_name, quantity, total, attendant_id, product_id))
-        return True
+        new_sale = Sales(product_id = product_id, total = total)
 
-    @staticmethod
-    def get_sales():
+        return self.db_con.create_sale(product_name, quantity, new_sale.total, attendant_id, new_sale.product_id)
+
+    def get_sales(self):
         """"
         method to return all sales made
         """
-        dict_cursor.execute("SELECT * FROM sales")
-        return dict_cursor.fetchall()
+        return self.db_con.get_sales()
 
-
-    @staticmethod
-    def get_single_sale(sale_id):
+    def get_single_sale(self, sale_id):
         """
         method to return single sale
         """
-        cursor.execute("SELECT * FROM sales WHERE sale_id=%s",(sale_id,))
-        return cursor.fetchone()
+        return self.db_con.get_single_sale(sale_id)
+
+    def update_on_sale(self, product_id, quantity):
+        """
+        method to update on sale (it takes on quantity only)
+        """
+        return self.db_con.update_on_sale(product_id, quantity)

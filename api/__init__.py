1467d3e2a716169c4cf60bc07746196bebe1ddc6
davidkaggulire <dkaggulire@gmail.com>
2018-11-16 01:56:10 +0300

    Adds flasgger to docs

--- api/__init__.py
+++ api/__init__.py
@@ -6,6 +6,7 @@ from flask_jwt_extended import JWTManager
 from api.views.product_views import product
 from api.views.sales_views import sales
 from api.views.user_views import userpage
+from flasgger import Swagger
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -13,12 +14,13 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config['JWT_SECRET_KEY'] = 'davidsecret123'
     JWTManager(app)
+    Swagger(app)
     from api.models.db import Database
     
     db = Database()
-    db.create_user_table()
-    db.create_products_table()
-    db.create_sales_table()
+    # db.create_user_table()
+    # db.create_products_table()
+    # db.create_sales_table()
 
     app.register_blueprint(product)
     app.register_blueprint(sales)

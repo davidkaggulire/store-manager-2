06b3ac0fe16d57eb9a74fcbc86565ed7e9ee3593
davidkaggulire <dkaggulire@gmail.com>
2018-11-19 10:38:49 +0300

    Ensure test pass

--- api/models/db.py
+++ api/models/db.py
@@ -13,13 +13,16 @@ class Database:
         try:
             if os.getenv('APP_SETTING') == 'testing':
                 db_name = "testdb"
+                user = "postgres"
+                pwd = "password"
+                host = "localhost"
+                port = "5432"
             elif os.getenv('heroku') == 'database':
                 db_name = "dd38125et4t431"
                 user = "tpzzndqodqzjda"
                 pwd = "0f0ff18502d303dc31bc3316b54eb5afd6fb44828d274238f7846708e9ee4c75"
                 host = "ec2-54-235-156-60.compute-1.amazonaws.com"
                 port = "5432"
-
             else:
                 db_name = "store"
                 user = "postgres"

064994473afe6865e757c7a0ee086b9766c4a1d9
davidkaggulire <dkaggulire@gmail.com>
2018-11-19 20:05:00 +0300

    [Bug]Adds production configuration

--- config.py
+++ config.py
@@ -20,10 +20,17 @@ class TestingConfig(Config):
     """
     DEBUG = True
     TESTING = True
-    DATABASE_URI = "postgresql://dkaggs:password@localhost/test"
+    DATABASE_URI = "postgresql://dkaggs:password@localhost/testdb"
 
+class ProductionConfig(Config):
+    """
+    Production config class
+    """
+    DEBUG = True
+    DATABASE_URI = "postgres://tpzzndqodqzjda:0f0ff18502d303dc31bc3316b54eb5afd6fb44828d274238f7846708e9ee4c75@ec2-54-235-156-60.compute-1.amazonaws.com:5432/dd38125et4t431"
 
 app_config = {
     'development': DevelopmentConfig,
-    'testing': TestingConfig
+    'testing': TestingConfig,
+    'production': ProductionConfig
 }
